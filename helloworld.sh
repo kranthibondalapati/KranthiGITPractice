cdfb7c21f059fb42b335016a172e13653b3d0bac
kranthibondalapati <bondalapati.kranthi@gmail.com>
2020-07-06 17:30:50 -0400

    helloworld

---
+++ helloworld.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+clear
+echo "Hello Shell World"
+
+echo "Hello Jaya"
+
+ps -ef
+
+echo "Hello $USER"
+echo "Today is \c ";date
+echo "Number of user login : \c" ; who | wc -l
+echo "Calendar"
+cal
+
